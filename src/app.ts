d7d8facce61598de3f5b3d0fc4638a0a9f60748f
Robinhood <okaforchidubem7@gmail.com>
2024-10-06 05:11:52 -0700

    changed from prod to dev

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 8000
 const app = express()
 
 app.use(cors({
-    origin: 'http://localhost:5173', // Replace with your frontend's origin
+    origin: process.env.NODE_ENV === 'development' ? 'http://localhost:5173' : '', // Replace with your frontend's origin
     credentials: true
   }));
 
